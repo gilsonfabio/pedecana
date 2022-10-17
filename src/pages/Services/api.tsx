2fe16c506628c69221cad10886b1ef355429568d
Gilson <gilsonfabio@gmail.com>
2022-10-16 22:33:20 -0300

    Atualizacoes

--- src/pages/Services/api.tsx
+++ src/pages/Services/api.tsx
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({
-    //baseURL: 'http://innvcar-com-br.umbler.net'
-    baseURL: 'http://10.111.134.40:3333'
-});
+const api = axios.create({baseURL: 'https://backend-sindicaldas.herokuapp.com' });
+//const api = axios.create({baseURL: 'http://localhost:3333' });
 
 export default api;
